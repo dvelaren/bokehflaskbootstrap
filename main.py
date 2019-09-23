ab5ea23e42b7995e6951af31f59d7d44786cfc87
David Velasquez <dvelasquez@ip-172-31-18-221.eu-west-2.compute.internal>
2019-09-23 09:50:34 +0000

    Added gitignore and docs files nginx and service

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-try:
-    import asyncio
-except ImportError:
-    raise RuntimeError("This example requries Python3 / asyncio")
-
 import logging
 
 from flask import Flask, render_template, url_for
 
-from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-
-from bokeh.application import Application
-from bokeh.application.handlers import FunctionHandler
 from bokeh.embed import server_document
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, Slider
 from bokeh.plotting import figure
-from bokeh.server.server import BaseServer
-from bokeh.server.tornado import BokehTornado
+from bokeh.server.server import Server
 from bokeh.server.util import bind_sockets
 from bokeh.themes import Theme
 
+'''
+flask_gunicorn_embed.py
+'''
+# try:
+#     import asyncio
+# except ImportError:
+#     raise RuntimeError("This example requries Python3 / asyncio")
+# from bokeh.application import Application
+# from bokeh.application.handlers import FunctionHandler
+# from bokeh.server.server import BaseServer
+# from bokeh.server.tornado import BokehTornado
+# from tornado.httpserver import HTTPServer
 
 app = Flask(__name__)
 
@@ -32,13 +34,7 @@ def modify_doc(doc):
     noise = np.random.normal(0,0.1,1000)
     y = 2*np.sin(x) + noise
     df = pd.DataFrame({'x':x,'y':y})
-    # df = sea_surface_temperature.copy()
     source = ColumnDataSource(data=df)
-
-    # plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
-    #               title="Sea Surface Temperature at 43.18, -70.43")
-    # plot.line('time', 'temperature', source=source)
-
     plot = figure(x_axis_label='Time',y_axis_label='Amplitude', y_range=(-0.5, 2.5))
     plot.line('x','y',source=source)
 
@@ -59,11 +55,13 @@ def modify_doc(doc):
 
 
 # can't use shortcuts here, since we are passing to low level BokehTornado
-bkapp = Application(FunctionHandler(modify_doc))
+# (flask_gunicorn_embed.py)
+# bkapp = Application(FunctionHandler(modify_doc))
 
 # This is so that if this app is run using something like "gunicorn -w 4" then
 # each process will listen on its own port
-sockets, port = bind_sockets("0.0.0.0", 0)
+# (flask_gunicorn_embed.py)
+# sockets, port = bind_sockets("0.0.0.0", 0)
 
 @app.route('/')
 @app.route('/home')
@@ -72,23 +70,25 @@ def home():
 
 @app.route('/graph', methods=['GET'])
 def graph():
+    script = server_document(url=r'/bkapp', relative_urls=True)
     # script = server_document('http://localhost:5006/bkapp')
-    script = server_document('http://siseflask.dis.eafit.edu.co:%d/bkapp' % port)
+    # script = server_document('http://siseflask.dis.eafit.edu.co:%d/bkapp' % port) # flask_gunicorn_embed.py
     return render_template("graph.html", script=script, template="Flask")
 
 def bk_worker():
     # # Can't pass num_procs > 1 in this configuration. If you need to run multiple
     # # processes, see e.g. flask_gunicorn_embed.py
-    # server = Server({'/bkapp': modify_doc}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
-    # server.start()
-    # server.io_loop.start()
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
-    bokeh_tornado = BokehTornado({'/bkapp': bkapp}, extra_websocket_origins=["127.0.0.1:5000","localhost:5000","0.0.0.0:5000","192.168.10.130","siseflask.dis.eafit.edu.co"])
-    bokeh_http = HTTPServer(bokeh_tornado)
-    bokeh_http.add_sockets(sockets)
-
-    server = BaseServer(IOLoop.current(), bokeh_tornado, bokeh_http)
+    '''
+    (flask_gunicorn_embed)
+    '''
+    # asyncio.set_event_loop(asyncio.new_event_loop())
+    # bokeh_tornado = BokehTornado({'/bkapp': bkapp}, extra_websocket_origins=["127.0.0.1:5000","localhost:5000","0.0.0.0:5000","192.168.10.130","siseflask.dis.eafit.edu.co"])
+    # bokeh_http = HTTPServer(bokeh_tornado)
+    # bokeh_http.add_sockets(sockets)
+    # server = BaseServer(IOLoop.current(), bokeh_tornado, bokeh_http)
+    kws = {'port': 5006, 'prefix': '/bkapp', 'allow_websocket_origin': ['ec2-3-10-15-221.eu-west-2.compute.amazonaws.com']}
+    # server = Server({'/bkapp': modify_doc}, io_loop=IOLoop(), allow_websocket_origin=["*"])
+    server = Server(modify_doc, io_loop=IOLoop(), **kws)
     server.start()
     server.io_loop.start()
 
@@ -101,21 +101,22 @@ if __name__ == '__main__':
     print('Multiple connections may block the Bokeh app in this configuration!')
     print('See "flask_gunicorn_embed.py" for one way to run multi-process')
     app.run(host='0.0.0.0', port=5000, debug=False)
-# else:
-	# gunicorn_logger = logging.getLogger('gunicorn.error')
-	# gunicorn_logger.setLevel(logging.INFO)
+
+# if __name__ != '__main__':
+# 	gunicorn_logger = logging.getLogger('gunicorn.error')
+# 	gunicorn_logger.setLevel(logging.INFO)
 	
-	# tornado_access_logger = logging.getLogger('tornado.access')
-	# tornado_access_logger.setLevel(logging.INFO)
-	# tornado_access_handler = logging.FileHandler('logs/error_log.log')
-	# tornado_access_logger.addHandler(tornado_access_handler)
+# 	tornado_access_logger = logging.getLogger('tornado.access')
+# 	tornado_access_logger.setLevel(logging.INFO)
+# 	tornado_access_handler = logging.FileHandler('logs/error_log.log')
+# 	tornado_access_logger.addHandler(tornado_access_handler)
 
-	# tornado_application_logger = logging.getLogger('tornado.application')
-	# tornado_application_logger.setLevel(logging.INFO)
-	# tornado_application_handler = logging.FileHandler('logs/error_log.log')
-	# tornado_application_logger.addHandler(tornado_application_handler)
+# 	tornado_application_logger = logging.getLogger('tornado.application')
+# 	tornado_application_logger.setLevel(logging.INFO)
+# 	tornado_application_handler = logging.FileHandler('logs/error_log.log')
+# 	tornado_application_logger.addHandler(tornado_application_handler)
 
-	# app.logger.addHandler(gunicorn_logger.handlers)
-	# app.logger.addHandler(tornado_access_logger.handlers)
-	# app.logger.addHandler(tornado_application_logger.handlers)
-	# app.logger.setLevel(logging.INFO)
+# 	app.logger.addHandler(gunicorn_logger.handlers)
+# 	app.logger.addHandler(tornado_access_logger.handlers)
+# 	app.logger.addHandler(tornado_application_logger.handlers)
+# 	app.logger.setLevel(logging.INFO)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dvelas25/bokehflaskbootstrap/venv"
+set -gx VIRTUAL_ENV "/home/dvelasquez/bokehflaskbootstrap/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
